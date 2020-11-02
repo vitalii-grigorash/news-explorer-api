254c382a14309f789fdcf1eb20a683b38e7a71ed
Vitalii Grigorash <vitalii.grigorash@yandex.ru>
2020-11-02 18:18:35 +0300

    added cors

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 require('dotenv').config();
-const express = require('express');
 const cors = require('cors');
+const express = require('express');
 const rateLimit = require('express-rate-limit');
 const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
@@ -19,11 +19,6 @@ const { DB_URL } = require('./config');
 const { PORT = 3000 } = process.env;
 const app = express();
 
-const corsOptions = {
-  origin: ['http://localhost:3000', 'https://localhost:3000', 'https://vitalii-grigorash.github.io/news-explorer-frontend/'],
-  credentials: true,
-};
-
 app.use(helmet());
 
 const limiter = rateLimit({
@@ -35,7 +30,8 @@ app.use(limiter);
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(cookieParser());
-app.use(cors(corsOptions));
+app.use(cors());
+app.options('*', cors());
 
 mongoose.connect(DB_URL, {
   useNewUrlParser: true,

09ab167025f71339eecd028d723ffc45a6951474
Vitalii Grigorash <vitalii.grigorash@yandex.ru>
2020-11-03 12:56:45 +0300

    change HttpOnly: false

--- controllers/users.js
+++ controllers/users.js
@@ -67,7 +67,7 @@ const login = (req, res, next) => {
       );
       res.cookie('jwt', token, {
         maxAge: 3600000,
-        httpOnly: true,
+        httpOnly: false,
         sameSite: false,
       })
         .end();
